e74459e9bb8054c061b7fbf4f5498d722dd5c157
priyanka <priyankarr140@gmail.com>
2025-07-11 12:10:10 +0530

    initial commit

---
+++ src/test/java/automation/coforge_jenkins/LoginTest.java
@@ -0,0 +1,11 @@
+package automation.coforge_jenkins;
+import org.testng.annotations.*;
+public class LoginTest {
+	
+	@Test
+	void login()
+	{
+		System.out.println("LOGIN TEST FUNCTIONALITY");
+	}
+
+}
